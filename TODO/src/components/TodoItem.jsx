ca850c4583bb6d39febdc39370341c81ed9567f0
shukrullahshirzad <shukrullah.shirzad.779@gmail.com>
2025-08-20 12:57:18 +0430

    add the complete task functionality

--- TODO/src/components/TodoItem.jsx
+++ TODO/src/components/TodoItem.jsx
@@ -3,12 +3,20 @@ import styles from "./TodoItem.module.css";
 export default function TodoItem({ item, todos, setTodos }) {
   function handleDelete(item) {
     console.log("Delete item:", item);
-    setTodos(todos.filter((todo) => todo !== item));
-  }
+      setTodos(todos.filter((todo) => todo !== item));
+      
+    }
+    function handleClick(name) {
+        console.log("Item clicked:", todos);
+        setTodos(todos.map(todo => 
+          todo.name === name ? {...todo, completed: !todo.completed} : todo
+        ));
+    }
+    const className = item.completed ? styles.completed : "";
   return (
     <div className={styles.item}>
       <div className={styles.itemName} key={item}>
-        {item.name}
+        <span className={className}  onClick={()=> handleClick(item.name)}>{item.name}</span>
         <button
           onClick={() => handleDelete(item)}
           className={styles.deleteButton}
