d429f2a0db8ba0428f1fbefd6b14512d2d44fa6c
shukrullahshirzad <shukrullah.shirzad.779@gmail.com>
2025-08-21 14:30:23 +0430

    add sorting functionality

--- TODO/src/components/TodoList.jsx
+++ TODO/src/components/TodoList.jsx
@@ -5,12 +5,19 @@ import styles from "./TodoList.module.css";
 
 // Define the TodoList component, receiving todos and setTodos as props
 export default function TodoList({ todos, setTodos }) {
+  const sortedTodos = todos.sort((a, b) => {
+    // Sort todos by completed status, then by name
+    if (a.completed === b.completed) {
+      return a.name.localeCompare(b.name); // Sort alphabetically if both have same completed status
+    }
+    return a.completed ? 1 : -1; // Completed todos come last
+  });
   // Render the list of TodoItem components
   return (
     <div className={styles.list}>
 
       {/* Container for the todo list */}
-      {todos.map((item) => (
+      {sortedTodos.map((item) => (
         <TodoItem
           key={item.name} // Unique key for each item
           item={item} // Pass the todo item
